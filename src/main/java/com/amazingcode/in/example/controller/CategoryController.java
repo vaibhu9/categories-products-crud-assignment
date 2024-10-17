35150931a38c793fee6cec93444b1a37f753630d
vaibhu9 <vaibhavshejol9@gmail.com>
2024-10-18 01:20:53 +0530

    all APIs for category is created

---
+++ src/main/java/com/amazingcode/in/example/controller/CategoryController.java
@@ -0,0 +1,55 @@
+package com.amazingcode.in.example.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amazingcode.in.example.entity.Category;
+import com.amazingcode.in.example.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+	
+	private final CategoryService categoryService;
+	
+	public CategoryController(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> createCategory(@RequestBody Category category){
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(category));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> getAllCategory(){
+		return ResponseEntity.status(HttpStatus.OK).body(categoryService.getAllCategory());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Category> getCategory(@PathVariable("id") Long id){
+		return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategory(id));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Category> updateCategory(@PathVariable("id") Long id, @RequestBody Category category){
+		return ResponseEntity.status(HttpStatus.OK).body(categoryService.updateCategory(id, category));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteCategory(@PathVariable("id") Long id){
+		categoryService.deleteCategory(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Category with id "+id+" deleted.");
+	}
+
+}
