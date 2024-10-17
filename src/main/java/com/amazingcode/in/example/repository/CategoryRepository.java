977828b90a73876fc4d2eb5dbe7ceee6350b669e
vaibhu9 <vaibhavshejol9@gmail.com>
2024-10-18 00:59:35 +0530

    Category repository created

---
+++ src/main/java/com/amazingcode/in/example/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.amazingcode.in.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.amazingcode.in.example.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
