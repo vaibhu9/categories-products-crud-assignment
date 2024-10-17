d2d42ec4ef386666b8cabe943d9382f8999682d8
vaibhu9 <vaibhavshejol9@gmail.com>
2024-10-18 00:28:34 +0530

    project structure created

---
+++ src/main/java/com/amazingcode/in/example/CategoriesProductsCrudAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.amazingcode.in.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriesProductsCrudAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoriesProductsCrudAssignmentApplication.class, args);
+	}
+
+}
