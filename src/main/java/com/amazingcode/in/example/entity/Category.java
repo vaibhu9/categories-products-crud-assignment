7414c0b82f38d937bd1421fd17736a2b0780c15a
vaibhu9 <vaibhavshejol9@gmail.com>
2024-10-18 00:55:56 +0530

    relationships established between entities

--- src/main/java/com/amazingcode/in/example/entity/Category.java
+++ src/main/java/com/amazingcode/in/example/entity/Category.java
@@ -1,9 +1,15 @@
 package com.amazingcode.in.example.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,4 +25,8 @@ public class Category {
 	private Long categoryId;
 	
 	private String categoryName;
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+	@JsonBackReference
+	private List<Product> products;
 }
--- src/main/java/com/amazingcode/in/example/entity/Product.java
+++ src/main/java/com/amazingcode/in/example/entity/Product.java
@@ -4,9 +4,12 @@ package com.amazingcode.in.example.entity;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,4 +31,8 @@ public class Product {
 	private LocalDate expiryDate;
 	
 	private Double price;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="category_id", nullable = false)
+	private Category category;
 }
